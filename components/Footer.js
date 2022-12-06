415460efe66697c7cc3533c1d578c95ed551f24b
SANDRO SIMON <sandrosimonore@gmail.com>
2022-12-06 01:49:50 -0500

    Footer menu added. No completed yet.

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,50 @@
 import styles from '../styles/Footer.module.css'
+
 export const Footer = () => {
+
+    const footerMenu = [
+        {
+            title: 'Support', 
+            menuItems: ['Help Center', 'AirCover', 'Supporting people with disabilities',
+                'Cancellation options', 'Our COVID-19 Response', 'Report a neighborhood concern'
+            ]
+        },
+        {
+            title: 'Community', 
+            menuItems: ['Airbnb.org: disaster relief housing', 'Combating discrimination']
+        },
+        {
+            title: 'Hosting',
+            menuItems: ['Airbnb your home', 'AirCover for Hosts', 'Explore hosting resources',
+            'Visit our community forum', 'How to host responsibly']
+        },
+        {
+            title: 'Airbnb', 
+            menuItems: ['Newsroom', 'Learn about new features', 'Letter from our founders',
+                'Careers', 'Investors', 'Gift cards']
+        }
+    ]
+
     return (
         <div className={styles.footer}>
-
+            
+            {
+                footerMenu.map( m => {
+                    return <div className={styles.menu}>
+                        <h6>{m.title}</h6>
+                        <ul>
+                        {
+                            m.menuItems.map( (e, i) => {
+                                return <li key={i}>{e}</li>
+                            } 
+                                 
+                            )
+                        }
+                        </ul>
+                    </div>
+                })
+            } 
+            
         </div>
     )
 }
