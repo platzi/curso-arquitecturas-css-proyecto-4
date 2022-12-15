793d7f4f3826f1c36c875197542679651ed3b611
SANDRO SIMON <sandrosimonore@gmail.com>
2022-12-15 01:17:58 -0500

    Sass Styles for RoomCard

--- components/RoomCard.js
+++ components/RoomCard.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from "next/image";
-import styles from '../styles/RoomCard.module.css'
+import styles from '../styles/RoomCard.module.scss'
 
 export const RoomCard = ({room}) => {
 
@@ -9,28 +9,25 @@ export const RoomCard = ({room}) => {
 
   return (
     <article className={styles.room}>
-      
-      <div className={styles.imgContainer}>
-        <Image
-          src={img_url}
-          width={500} 
-          height={500}
-          alt="" 
-        />
-        <div className={styles.like}>
-          <p>Like!</p>
+      <Link href={`/room/${slug}`} className={styles.roomLink}>
+        <div className={styles.imgContainer}>
+          <Image
+            src={img_url}
+            width={500} 
+            height={500}
+            alt="" 
+          />
+          <div className={styles.like}>
+            <p>Like!</p>
+          </div>
         </div>
-        <Link href={`/room/${slug}`} className={`${styles.roomLink} btn-primary`}>
-          features
-        </Link>
-      </div>
-      <div>
-        <h3>{name}</h3>
-        <p>Cusco Perú</p>
-        <span>4.94</span>
-        <p>S/ {price} per night </p>
-      </div>
-      
+        <div>
+          <h3>{name}</h3>
+          <p>Cusco Perú</p>
+          <span>4.94</span>
+          <p>S/ {price} per night </p>
+        </div>
+      </Link>      
     </article>
   );
 };
