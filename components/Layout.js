7c8b4a4eb816929b3ed7b946f9154c01b92a295f
SANDRO SIMON <sandrosimonore@gmail.com>
2023-01-24 16:39:22 -0500

    Head added

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import { Header } from "./Header"
 import { Footer } from "./Footer"
 import styles from "../styles/Layout.module.scss"
@@ -6,6 +7,14 @@ export const Layout = ( {children} ) => {
 
     return (
         <div className={styles.layout}>
+            <Head>
+                <title>Beautiful Places</title>
+                <meta property="og:title" content="Beautiful Places" key="title" />
+                <meta name='type' property='og:type' content='website' />
+                <meta name='image' property='og:image' content='https://places.sandrosimon.com/images/preview.jpg' />
+                <meta name='url' property='og:url' content='https://places.sandrosimon.com' />
+                <meta name="description" property='og:description' content="Places is a website where you can look for a place to stay by yourself or with your family and reserve the one you like." />
+            </Head>
             <div className={styles.placeholder}></div>
             <Header />
             <main className={styles.main}>{children}</main>
