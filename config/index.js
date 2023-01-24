81820906bcb61a2e43d04187ae6069afef80bc81
SANDRO SIMON <sandrosimonore@gmail.com>
2023-01-24 17:22:13 -0500

    config fixed

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000/' : 'https://resort-eight.vercel.app/';
+export const server = dev ? 'http://localhost:3000/' : 'https://places.sandrosimon.com/';
 
 export const mongoDbUri = process.env.MONGODB_URI
 
