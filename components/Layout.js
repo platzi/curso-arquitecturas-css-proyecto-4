ee1754586baa70693462b9eff46a4c63140b9c8e
SANDRO SIMON <sandrosimonore@gmail.com>
2023-01-24 17:35:28 -0500

    Meta info (HEAD) added back

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-//import Head from "next/head"
+import Head from "next/head"
 import { Header } from "./Header"
 import { Footer } from "./Footer"
 import styles from "../styles/Layout.module.scss"
@@ -7,7 +7,14 @@ export const Layout = ( {children} ) => {
 
     return (
         <div className={styles.layout}>
-            
+            <Head>
+                <title>Beautiful Places</title>
+                <meta property="og:title" content="Beautiful Places" key="title" />
+                <meta name='type' property='og:type' content='website' />
+                <meta name='image' property='og:image' content='https://places.sandrosimon.com/images/preview.png' />
+                <meta name='url' property='og:url' content='https://places.sandrosimon.com' />
+                <meta name="description" property='og:description' content="Places is a website where you can look for a place to stay by yourself or with your family and reserve the one you like." />
+            </Head> 
             <div className={styles.placeholder}></div>
             <Header />
             <main className={styles.main}>{children}</main>
