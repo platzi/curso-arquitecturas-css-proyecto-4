8d0008bd0f38cad7738666a9fd309ab06c6bd7ef
SANDRO SIMON <sandrosimonore@gmail.com>
2023-01-24 16:50:08 -0500

    More metas removed...

--- components/Layout.js
+++ components/Layout.js
@@ -10,10 +10,10 @@ export const Layout = ( {children} ) => {
             <Head>
                 <title>Beautiful Places</title>
                 <meta property="og:title" content="Beautiful Places" key="title" />
-                <meta name='type' property='og:type' content='website' />
-                {/* <meta name='image' property='og:image' content='https://places.sandrosimon.com/images/preview.png' /> */}
+                {/* <meta name='type' property='og:type' content='website' />
+                <meta name='image' property='og:image' content='https://places.sandrosimon.com/images/preview.png' />
                 <meta name='url' property='og:url' content='https://places.sandrosimon.com' />
-                <meta name="description" property='og:description' content="Places is a website where you can look for a place to stay by yourself or with your family and reserve the one you like." />
+                <meta name="description" property='og:description' content="Places is a website where you can look for a place to stay by yourself or with your family and reserve the one you like." /> */}
             </Head>
             <div className={styles.placeholder}></div>
             <Header />
