692f610aef3f8bbb9a3456dd13f1a0780ff2bee8
SANDRO SIMON <sandrosimonore@gmail.com>
2023-02-20 17:13:10 -0500

    style signup page

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
+import Link from 'next/link';
 import { signIn, useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import styles from '../../styles/SignupPage.module.scss'
 
 export default function SignupPage() {
 
@@ -47,110 +49,114 @@ export default function SignupPage() {
   };
 
   return (
-    <div>
-      <form
-        className=""
-        onSubmit={handleSubmit(submitHandler)}
-      >
-        {/** First name */}
-        <div className="">
-          <input
-            {
-                ...register('firstName', {
-                    required: 'Enter your first name',
-                    pattern: { value: /^[a-zA-Z_.+-]+$/i, message: 'Please enter only letters'},
-                })
-            }
-            type="text"
-            className=""
-            placeholder='Enter your first name'
-            autoFocus
-          />
-          {errors.firstName && ( <small className="">{errors.firstName.message}</small> )}
+    <div className={styles.signupPage}>
+      <div className={styles.formContainer}>
+        <h2 className="">Sign up</h2>
+        <p>Please, fill the form below to get an account.</p>
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit(submitHandler)}
+        >
+          {/** First name */}
+          <div className="">
+            <input
+              {
+                  ...register('firstName', {
+                      required: 'Enter your first name',
+                      pattern: { value: /^[a-zA-Z_.+-]+$/i, message: 'Please enter only letters'},
+                  })
+              }
+              type="text"
+              className=""
+              placeholder='Enter your first name'
+              autoFocus
+            />
+            {errors.firstName && ( <small className="">{errors.firstName.message}</small> )}
 
-        </div>
+          </div>
 
-        {/** Last name */}
-        <div className="">
-          <input
-            {
-                ...register('lastName', {
-                    required: 'Enter your last name',
-                    pattern: {
-                        value: /^[a-zA-Z_.+-]+$/i,
-                        message: 'Please enter only letters',
-                    },
-                })
-            }
-            type="text"
-            className=""
-            placeholder='Enter your last name'
-            autoFocus
-          />
-          {errors.lastName && ( <small className="">{errors.lastName.message}</small> )}
+          {/** Last name */}
+          <div className="">
+            <input
+              {
+                  ...register('lastName', {
+                      required: 'Enter your last name',
+                      pattern: {
+                          value: /^[a-zA-Z_.+-]+$/i,
+                          message: 'Please enter only letters',
+                      },
+                  })
+              }
+              type="text"
+              className=""
+              placeholder='Enter your last name'
+              autoFocus
+            />
+            {errors.lastName && ( <small className="">{errors.lastName.message}</small> )}
 
-        </div>
-        
-        {/** Email */}
-        <div className="">
-          <input
-            type="email"
-            {
-                ...register('email', {
-                    required: 'Please enter an email',
-                    pattern: {
-                        value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                        message: 'Please enter a valid email',
-                    },
-                })
-            }
-            placeholder='Enter an email'
-            className=""
-            id="email"
-            autoFocus
-          />
-          {errors.email && (
-            <small className="">{errors.email.message}</small>
-          )}
+          </div>
+          
+          {/** Email */}
+          <div className="">
+            <input
+              type="email"
+              {
+                  ...register('email', {
+                      required: 'Please enter an email',
+                      pattern: {
+                          value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                          message: 'Please enter a valid email',
+                      },
+                  })
+              }
+              placeholder='Enter an email'
+              className=""
+              id="email"
+              autoFocus
+            />
+            {errors.email && (
+              <small className="">{errors.email.message}</small>
+            )}
 
-        </div>
-        
-        {/** Password */}
-        <div className="">
-          <input
-            {...register('password', { required: 'Please enter password' })}
-            type="password"
-            className=""
-            id="password"
-            placeholder='Enter a password'
-          />
-          {errors.password && (
-            <small className="">{errors.password.message}</small>
-          )}
-        </div>
+          </div>
+          
+          {/** Password */}
+          <div className="">
+            <input
+              {...register('password', { required: 'Please enter password' })}
+              type="password"
+              className=""
+              id="password"
+              placeholder='Enter a password'
+            />
+            {errors.password && (
+              <small className="">{errors.password.message}</small>
+            )}
+          </div>
 
-        {/** Confirm Password */}
-        <div className="">
-          <input
-            {...register('confirmPassword', { required: 'Please confirm password', validate: v => v === getValues('password') })}
-            type="password"
-            className=""
-            id="confirmPassword"
-            placeholder='Confirm your password'
-          />
-          {errors.password && (
-            <small className="">{errors.password.message}</small>
-          )}
-        </div>
+          {/** Confirm Password */}
+          <div className="">
+            <input
+              {...register('confirmPassword', { required: 'Please confirm password', validate: v => v === getValues('password') })}
+              type="password"
+              className=""
+              id="confirmPassword"
+              placeholder='Confirm your password'
+            />
+            {errors.password && (
+              <small className="">{errors.password.message}</small>
+            )}
+          </div>
 
-        <div className="">
-          <button className="">Login</button>
-        </div>
+          
+          <button className="">Sign up</button>
+          
 
-      </form>
-      <div>
-        Already have an account? 
+        </form>
+        <p>Already have an account?</p>
+        <Link href='/login'>Sign in here</Link>
       </div>
+      
     </div>
   );
 }
